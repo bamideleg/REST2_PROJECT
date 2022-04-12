3be3bad9682ef5ab3e26f0de2e89f764e3b6c99f
bamideleg <bamideleg74@gmail.com>
2022-04-12 03:18:06 +0100

    Added API regression Pack

---
+++ src/test/java/REST_PROJECT/Test_PUT.java
@@ -0,0 +1,36 @@
+package REST_PROJECT;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test_PUT {
+
+
+    @Test
+    public void test_POST1(){
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Lord Nelson");
+        request.put("Job", "Teacher");
+
+        // Different way fo printing values
+        System.out.println(request);
+        System.out.println(request.toJSONString());
+
+
+        given().
+                header("content-type", "application/json").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+
+                when().
+                         put("https://reqres.in/api/users/2").
+                then().statusCode(200).log().all();
+    }
+
+}
